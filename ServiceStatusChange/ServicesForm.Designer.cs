713579decae1af7117322a8f2a75852bef18e9bb
Erdem TUZEN <erdemtuzen@gmail.com>
2015-03-09 16:40:01 +0200

    First Upload

---
+++ ServiceStatusChange/ServicesForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ServiceStatusChange
+{
+    partial class ServicesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ServicesForm));
+            this.ServicesDataGridView = new System.Windows.Forms.DataGridView();
+            this.InfoStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.EnabledButton = new System.Windows.Forms.Button();
+            this.DisabledButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.RestartButton = new System.Windows.Forms.Button();
+            this.StatusToolStripProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.ServicesDataGridView)).BeginInit();
+            this.InfoStatusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ServicesDataGridView
+            // 
+            this.ServicesDataGridView.AllowUserToAddRows = false;
+            this.ServicesDataGridView.AllowUserToDeleteRows = false;
+            this.ServicesDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ServicesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ServicesDataGridView.Location = new System.Drawing.Point(12, 12);
+            this.ServicesDataGridView.Name = "ServicesDataGridView";
+            this.ServicesDataGridView.ReadOnly = true;
+            this.ServicesDataGridView.Size = new System.Drawing.Size(624, 205);
+            this.ServicesDataGridView.TabIndex = 0;
+            // 
+            // InfoStatusStrip
+            // 
+            this.InfoStatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.StatusToolStripStatusLabel,
+            this.StatusToolStripProgressBar});
+            this.InfoStatusStrip.Location = new System.Drawing.Point(0, 252);
+            this.InfoStatusStrip.Name = "InfoStatusStrip";
+            this.InfoStatusStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.InfoStatusStrip.Size = new System.Drawing.Size(649, 22);
+            this.InfoStatusStrip.TabIndex = 1;
+            this.InfoStatusStrip.Text = "Info";
+            // 
+            // EnabledButton
+            // 
+            this.EnabledButton.Location = new System.Drawing.Point(12, 223);
+            this.EnabledButton.Name = "EnabledButton";
+            this.EnabledButton.Size = new System.Drawing.Size(120, 23);
+            this.EnabledButton.TabIndex = 2;
+            this.EnabledButton.Text = "Service(s) Enable";
+            this.EnabledButton.UseVisualStyleBackColor = true;
+            this.EnabledButton.Click += new System.EventHandler(this.EnabledButton_Click);
+            // 
+            // DisabledButton
+            // 
+            this.DisabledButton.Location = new System.Drawing.Point(138, 223);
+            this.DisabledButton.Name = "DisabledButton";
+            this.DisabledButton.Size = new System.Drawing.Size(120, 23);
+            this.DisabledButton.TabIndex = 3;
+            this.DisabledButton.Text = "Service(s) Disable";
+            this.DisabledButton.UseVisualStyleBackColor = true;
+            this.DisabledButton.Click += new System.EventHandler(this.DisabledButton_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(264, 223);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(120, 23);
+            this.StartButton.TabIndex = 4;
+            this.StartButton.Text = "Service(s) Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(390, 223);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(120, 23);
+            this.StopButton.TabIndex = 5;
+            this.StopButton.Text = "Service(s) Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // RestartButton
+            // 
+            this.RestartButton.Location = new System.Drawing.Point(516, 223);
+            this.RestartButton.Name = "RestartButton";
+            this.RestartButton.Size = new System.Drawing.Size(120, 23);
+            this.RestartButton.TabIndex = 6;
+            this.RestartButton.Text = "Service(s) Restart";
+            this.RestartButton.UseVisualStyleBackColor = true;
+            this.RestartButton.Click += new System.EventHandler(this.RestartButton_Click);
+            // 
+            // StatusToolStripProgressBar
+            // 
+            this.StatusToolStripProgressBar.Name = "StatusToolStripProgressBar";
+            this.StatusToolStripProgressBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(42, 17);
+            this.toolStripStatusLabel1.Text = "Status:";
+            // 
+            // StatusToolStripStatusLabel
+            // 
+            this.StatusToolStripStatusLabel.Name = "StatusToolStripStatusLabel";
+            this.StatusToolStripStatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // ServicesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 274);
+            this.Controls.Add(this.RestartButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.DisabledButton);
+            this.Controls.Add(this.EnabledButton);
+            this.Controls.Add(this.InfoStatusStrip);
+            this.Controls.Add(this.ServicesDataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ServicesForm";
+            this.Text = "Service Status Enable/Disable";
+            this.Load += new System.EventHandler(this.ServicesForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ServicesDataGridView)).EndInit();
+            this.InfoStatusStrip.ResumeLayout(false);
+            this.InfoStatusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ServicesDataGridView;
+        private System.Windows.Forms.StatusStrip InfoStatusStrip;
+        private System.Windows.Forms.Button EnabledButton;
+        private System.Windows.Forms.Button DisabledButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button RestartButton;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar StatusToolStripProgressBar;
+        private System.Windows.Forms.ToolStripStatusLabel StatusToolStripStatusLabel;
+    }
+}
+
---
+++ ServiceStatusChange/ServicesForm.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.ServiceProcess;
+using Microsoft.Win32;
+
+namespace ServiceStatusChange
+{
+    public partial class ServicesForm : Form
+    {
+        public ServicesForm()
+        {
+            InitializeComponent();
+        }
+
+        BackgroundWorker backGroundWorker = new BackgroundWorker();
+        public WindowsServiceMonitor _windowsServiceMonitor { get; set; }
+
+        private void ServicesForm_Load(object sender, EventArgs e)
+        {
+            FillServices();
+            StatusToolStripProgressBar.Visible = false;
+        }
+
+        private void FillServices()
+        {
+            ServicesDataGridView.Rows.Clear();
+            ServicesDataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            ServicesDataGridView.ColumnCount = 3;
+            ServicesDataGridView.Columns[0].Name = "Name";
+            ServicesDataGridView.Columns[0].Width = 125;
+            ServicesDataGridView.Columns[1].Name = "Display Name";
+            ServicesDataGridView.Columns[1].Width = 350;
+            ServicesDataGridView.Columns[2].Name = "Status";
+
+            ServiceController[] services = ServiceController.GetServices();
+            string[] row;
+            foreach (ServiceController service in services)
+            {
+                if (service.ServiceName.Contains("SP"))
+                {
+                    row = new string[] { service.ServiceName, service.DisplayName, service.Status.ToString() };
+                    ServicesDataGridView.Rows.Add(row);
+                }
+            }
+
+        }
+
+        private void ChangeServiceStatus(string status)
+        {
+            try
+            {
+                string message = string.Empty;
+                StatusToolStripStatusLabel.Text = "Please wait";
+                StatusToolStripProgressBar.Visible = true;
+
+                ProgressBar();
+                foreach (DataGridViewRow row in ServicesDataGridView.SelectedRows)
+                {
+                    
+                    _windowsServiceMonitor = new WindowsServiceMonitor(row.Cells[0].Value.ToString());
+                    switch (status)
+                    {
+                        case "Start": _windowsServiceMonitor.Start();
+                            break;
+                        case "Stop": _windowsServiceMonitor.Stop();
+                            break;
+                        case "Restart": _windowsServiceMonitor.Restart();
+                            break;
+                        case "Disable": _windowsServiceMonitor.Disable();
+                            break;
+                        case "Enable": _windowsServiceMonitor.Enable();
+                            break;
+                        default:
+                            break;
+                    }
+                    FillServices();
+                    
+                    message += row.Cells[0].Value.ToString() + ", ";
+
+                }
+                StatusToolStripStatusLabel.Text = string.Format("{0} service(s) status changed.", message);
+
+            }
+            catch (Exception ex)
+            {
+                StatusToolStripStatusLabel.Text = ex.Message;
+            }
+        }
+
+        private void ProgressBar()
+        {
+            backGroundWorker.RunWorkerAsync();
+
+            backGroundWorker.DoWork += new DoWorkEventHandler(backGroundWorker_DoWork);
+            backGroundWorker.ProgressChanged += new ProgressChangedEventHandler(backGroundWorker_ProgressChanged);
+            backGroundWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backGroundWorker_RunWorkerCompleted);
+            backGroundWorker.WorkerReportsProgress = true;
+
+        }
+
+        void backGroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            StatusToolStripProgressBar.Visible = false;
+        }
+
+        void backGroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            StatusToolStripProgressBar.Value = e.ProgressPercentage;
+        }
+
+        void backGroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                backGroundWorker.ReportProgress(i);
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
+        private void EnabledButton_Click(object sender, EventArgs e)
+        {
+            ChangeServiceStatus("Enable");
+        }
+
+        private void DisabledButton_Click(object sender, EventArgs e)
+        {
+            ChangeServiceStatus("Disable");
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            ChangeServiceStatus("Start");
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            ChangeServiceStatus("Stop");
+        }
+
+        private void RestartButton_Click(object sender, EventArgs e)
+        {
+            ChangeServiceStatus("Restart");
+        }
+    }
+}
